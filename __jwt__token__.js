9be80a4de1b7d2547b2bb063b2a69e53995119fa
mamun-prodhan <mamunpr9@gmail.com>
2023-11-02 23:28:23 +0600

    jwt implemented

--- __jwt__token__.js
+++ __jwt__token__.js
@@ -19,3 +19,11 @@
  * 4.from client side: axios withCredentials true
  * 5. cors setup origin and credentials: true
  */
+
+/***
+ * 1. for secure api calls
+ * 2. server side: install cookie parser and use it as a middleware
+ * 3. req.cookies
+ * 4. on the client side: make api call using axios withCredentials: true
+ * (or credentials include while using fetch)
+ */
