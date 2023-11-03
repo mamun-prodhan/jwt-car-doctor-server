f9f7231a252045e8b56e51bf02a1bccfaa70e070
mamun-prodhan <mamunpr9@gmail.com>
2023-11-04 01:03:57 +0600

    bug fixed- token will delete after logout

--- index.js
+++ index.js
@@ -74,8 +74,10 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: true,
-          sameSite: "none",
+          // secure: true,
+          secure: process.env.NODE_ENV === "production",
+          // sameSite: "none",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
@@ -83,7 +85,13 @@ async function run() {
     app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("logging out", user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
     // services related api
